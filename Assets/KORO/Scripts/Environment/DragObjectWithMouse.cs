544966503d169984c95f0a9e88999827bd67a528
DESKTOP-7QEULK1\USER <meenesalnet@gmail.com>
2024-06-08 01:52:32 +0300

    fix:drag object

---
+++ Assets/KORO/Scripts/Environment/DragObjectWithMouse.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class DragObjectWithMouse : MonoBehaviour
+{
+    public Transform objectToMove; // Hareket ettirilecek obje
+    private bool isDragging = false;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        // E tuşuna basıldığında hareket modunu aktif et/deaktif et
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            isDragging = !isDragging;
+        }
+        if (Input.GetKeyUp(KeyCode.H))
+        {
+            isDragging = false;
+        }
+        
+
+        // Hareket modu aktifse, fareyi takip et ve objeyi taşı
+        if (isDragging)
+        {
+            MoveObjectToMousePosition();
+        }
+    }
+
+    void MoveObjectToMousePosition()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            objectToMove.position = hit.point; // Objenin pozisyonunu fare ile tıklanan noktaya taşı
+        }
+    }
+}
