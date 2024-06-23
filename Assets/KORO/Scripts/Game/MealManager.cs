f12145ac027dc4e683a8bbab85e52f5198fee32f
DESKTOP-7QEULK1\USER <meenesalnet@gmail.com>
2024-06-23 20:10:53 +0300

    fix: food ingredient

--- Assets/KORO/Scripts/Game/MealManager.cs
+++ Assets/KORO/Scripts/Game/MealManager.cs
@@ -57,9 +57,16 @@ public class MealManager : MonoBehaviour
     public void MakeMeal(Enums.OrderType mealName,int value)
     {
         Meal meal = mealsList.meals.Find(m => m.mealName == mealName);
-        if (meal != null && meal.ingredientQuantity > 0)
+        if (meal != null )
         {
-            meal.ingredientQuantity += value;
+            if (value < 0 && meal.ingredientQuantity > 0)
+            {
+                meal.ingredientQuantity += value;
+            }
+            else if(value > 0)
+            {
+                meal.ingredientQuantity += value;
+            }
             PlayerPrefsManager.Instance.SaveMeals(mealsList);
             Debug.Log("Made " + mealName + ". Remaining quantity: " + meal.ingredientQuantity);
         }
