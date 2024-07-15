25cd6295af31d400be20c4f78c225573131a7bc2
DESKTOP-7QEULK1\USER <meenesalnet@gmail.com>
2024-07-15 14:40:36 +0300

    fix: orderbox

--- Assets/KORO/Scripts/Environment/OrderBox.cs
+++ Assets/KORO/Scripts/Environment/OrderBox.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
-public class OrderBox : MonoBehaviour
+public class OrderBox : MonoBehaviour,IInterectableObject
 {
     [SerializeField] private ShopItemData _shopItemData;
     // Start is called before the first frame update
@@ -20,5 +21,32 @@ public class OrderBox : MonoBehaviour
     public void SetShopItemData(ShopItemData shopItemData)
     {
         _shopItemData = shopItemData;
+    }
+
+    public void InterectableObjectRun()
+    {
+        transform.DOLocalMove(new Vector3(-0.1f,-0.2f,0f), 0.2f);
+        transform.DOLocalRotate(new Vector3(0,0,-11f), 0.2f);
+        transform.SetParent(CameraController.Instance.PlayerTakedObjectTransformParent);
+    }
+
+    public void ShowOutline(bool active)
+    {
+        
+    }
+
+    public Outline GetOutlineComponent()
+    {
+        return null;
+    }
+
+    public string GetInterectableText()
+    {
+        return null;
+    }
+
+    public void Move()
+    {
+        
     }
 }
