bf4602912e0b0bbc5016b178361111aeafb4ad1e
DESKTOP-7QEULK1\USER <meenesalnet@gmail.com>
2024-08-11 01:15:34 +0300

    feat: decorationmove

--- Assets/KORO/Scripts/Environment/DecorationObject.cs
+++ Assets/KORO/Scripts/Environment/DecorationObject.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DecorationObject : MonoBehaviour,IInterectableObject
 {
+    private bool isDecorationMove;
+
+    private void Update()
+    {
+        if (isDecorationMove)
+        {
+            MoveStart();
+        }
+    }
+
     public void InterectableObjectRun()
     {
         
@@ -33,6 +44,58 @@ public class DecorationObject : MonoBehaviour,IInterectableObject
     }
     public void Move()
     {
+        if (!isDecorationMove && !PlaceController.RestaurantIsOpen)
+        {
+            isDecorationMove = true;
+            Player.Instance.PlayerTakedObject = gameObject;
+            //Player.Instance.PlayerStateType = Enums.PlayerStateType.MoveTable;
+            //GameSceneCanvas.Instance.ShowAreaInfoForTexts(textsForTable);
+            //GameSceneCanvas.Instance.ShowAreaInfoForTextsButtons(textsButtonsForTable);
+        }
+    }
+    public void MoveStart()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            float xValue = hit.point.x;
+            float zValue = hit.point.z;
+            GetComponent<BoxCollider>().enabled = true;
+            transform.position = new Vector3(xValue,0,zValue); // Objenin pozisyonunu fare ile tıklanan noktaya taşı
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            var tableRotat = transform.rotation;
+            var tableRotatTemp = Quaternion.Euler(new Vector3(tableRotat.eulerAngles.x,tableRotat.eulerAngles.y+90f,tableRotat.eulerAngles.z));
+
+            transform.rotation = tableRotatTemp;
+        }
+        /*if (Input.GetMouseButton(0))
+        {
+            TableSet.CheckGround();
+            if (IsTableSetTransform)
+            {
+                MapManager.Instance.SaveMap();
+                TableControl();
+                Player.Instance.PlayerStateType = Enums.PlayerStateType.Free;
+                //if(colliders.Length )
+                GetComponent<BoxCollider>().enabled = false;
+                TableController.Instance.EnableTableSetCollider(false);
+                IsTableSetTransform = false;
+                IsTableMove = false;
+                Player.Instance.PlayerTakedObject = null;
+                if (PlayerPrefsManager.Instance.LoadPlayerTutorialStep() == 2)
+                {
+                    PlayerPrefsManager.Instance.SavePlayerPlayerTutorialStep(4);
+                    TutorialManager.Instance.Initiliaze();
+                }
+            }
+            
+        }*/
         
     }
     public void Open()
