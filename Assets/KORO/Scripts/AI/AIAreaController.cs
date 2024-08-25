08512abdcb964a2818b1f680053ccbdcc4a8732f
DESKTOP-7QEULK1\USER <meenesalnet@gmail.com>
2024-08-25 18:01:47 +0300

    fix: aiareacontroller

--- Assets/KORO/Scripts/AI/AIAreaController.cs
+++ Assets/KORO/Scripts/AI/AIAreaController.cs
@@ -18,61 +18,10 @@ public class AIAreaController : MonoBehaviour
         gameObject.TryGetComponent(out WaiterController);
     }
 
-    void Start()
-    {
-        
-    }
+    
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        /*if (!Interactabled)
-        {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, 0.2f);
-
-            foreach (Collider col in colliders)
-            {
-                var interfacezort = col.gameObject.GetComponent<IAIInteractable>();
-                if (interfacezort != null)
-                {
-                    interfacezort.StartState(transform);
-                }
-                else
-                {
-                
-                }
-            } 
-        }*/
-    }
-
-    public void StartInteractableObject(Enums.AIStateType aiStateType)
-    {
-        if (!Interactabled)
-        {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, 0.2f);
-
-            foreach (Collider col in colliders)
-            {
-                if (aiStateType == Enums.AIStateType.Customer)
-                {
-                    var interfacezort = col.gameObject.GetComponent<IAIInteractable>();
-                    if (interfacezort != null)
-                    {
-                        interfacezort.StartState(this,Enums.AIStateType.Customer);
-                    }
-                }
-                else if (aiStateType == Enums.AIStateType.Waiter)
-                {
-                    var interfacezort = col.gameObject.GetComponent<IAIInteractable>();
-                    if (interfacezort != null)
-                    {
-                        interfacezort.StartState(this,Enums.AIStateType.Waiter);
-                    }
-                }
-                
-            } 
-        }
-    }
+    
     
 
     public void InteractabelControl()
