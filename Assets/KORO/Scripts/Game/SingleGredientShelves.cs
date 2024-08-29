a040ba882ee67905dcf0ff79f64b0cf5129d28d7
DESKTOP-7QEULK1\USER <meenesalnet@gmail.com>
2024-08-29 21:34:47 +0300

    feat: shelve script

---
+++ Assets/KORO/Scripts/Game/SingleGredientShelves.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleGredientShelves : MonoBehaviour,IInterectableObject
+{
+    public Enums.FoodIngredientType shelveIngredientType;
+
+    public void InterectableObjectRun()
+    {
+        
+    }
+
+    public void ShowOutline(bool active)
+    {
+        
+    }
+
+    public Outline GetOutlineComponent()
+    {
+        return null;
+
+    }
+    public string GetInterectableText()
+    {
+        return null;
+
+    }
+    public string[] GetInterectableTexts()
+    {
+        return null;
+
+    }
+    public string[] GetInterectableButtons()
+    {
+        return null;
+    }
+    public void Move()
+    {
+    
+    }
+    public void Open()
+    {
+    
+    }
+    public Enums.PlayerStateType GetStateType()
+    {
+        return Enums.PlayerStateType.Free;
+    }
+}
