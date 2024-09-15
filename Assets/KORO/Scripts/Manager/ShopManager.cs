58fd558d2b1915c71b0af881d20ed67a286ea293
DESKTOP-7QEULK1\USER <meenesalnet@gmail.com>
2024-09-15 22:10:17 +0300

    fix: totaltext

--- Assets/KORO/Scripts/Manager/ShopManager.cs
+++ Assets/KORO/Scripts/Manager/ShopManager.cs
@@ -181,6 +181,7 @@ public class ShopManager : MonoBehaviour
             }
             ShoppingBasket.Clear();
         }
+        PlacePanelController.Instance.UpdateCostText();
         
     }
 
