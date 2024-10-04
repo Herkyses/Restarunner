7c02bfb020b6769514cf89518865b85a0b5a9744
DESKTOP-7QEULK1\USER <meenesalnet@gmail.com>
2024-10-04 22:37:05 +0300

    fix: daynight cycle

--- Assets/KORO/Scripts/Manager/DayNightCycle.cs
+++ Assets/KORO/Scripts/Manager/DayNightCycle.cs
@@ -12,11 +12,20 @@ public class DayNightCycle : MonoBehaviour
     public Color ambientDayColor;
     public Color ambientNightColor;
     public float dayDuration = 60f;  // Bir gün kaç saniye sürecek
+    public bool StartCycle = false; 
     
     private float currentTime = 0f;
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            InitiliazeCycle();
+        }
+        if (!StartCycle)
+        {
+            return;
+        }
         currentTime += Time.deltaTime;
         float timePercent = (currentTime % dayDuration) / dayDuration;
 
@@ -25,20 +34,35 @@ public class DayNightCycle : MonoBehaviour
         directionalLight.intensity = lightIntensity.Evaluate(timePercent);
 
         // Ortam ışığını değiştirme
-        if (timePercent < 0.5f)
+        /*if (timePercent < 0.5f)
         {
             RenderSettings.ambientLight = Color.Lerp(ambientNightColor, ambientDayColor, timePercent * 2f);
         }
         else
         {
             RenderSettings.ambientLight = Color.Lerp(ambientDayColor, ambientNightColor, (timePercent - 0.5f) * 2f);
-        }
-
-        // Post Process ayarlarını değiştir
+        }*/
         if (postProcessVolume.profile.TryGetSettings(out ColorGrading colorGrading))
         {
             colorGrading.temperature.value = Mathf.Lerp(-10f, 10f, timePercent);
             colorGrading.saturation.value = Mathf.Lerp(-30f, 20f, timePercent);
         }
+        if (timePercent > 0.5f)
+        {
+            RenderSettings.ambientLight = Color.Lerp(ambientDayColor, ambientNightColor, (timePercent - 0.5f) * 2f);
+        }
+        else
+        {
+            StartCycle = false;
+        }
+
+        // Post Process ayarlarını değiştir
+        
+    }
+
+    public void InitiliazeCycle()
+    {
+        StartCycle = true;
+        currentTime = dayDuration / 2;
     }
 }
