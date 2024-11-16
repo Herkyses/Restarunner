7f9ac13f76e9728427489e54f00877e8b008e2a6
DESKTOP-7QEULK1\USER <meenesalnet@gmail.com>
2024-11-16 23:46:59 +0300

    fix:itemtext

--- Assets/KORO/Scripts/Game/PlayerItemController.cs
+++ Assets/KORO/Scripts/Game/PlayerItemController.cs
@@ -6,18 +6,22 @@ using UnityEngine;
 public class PlayerItemController : MonoBehaviour
 {
     [SerializeField] private Transform _itemsParent;
-    [SerializeField] private List<TextMeshProUGUI> _itemTexts;
+    [SerializeField] private List<string> _itemTexts;
     [SerializeField] private List<Sprite> _itemSprites;
     [SerializeField] private SingleItemInfo _singleItemInfoPf;
     // Start is called before the first frame update
     void Start()
     {
         Utilities.DeleteTransformchilds(_itemsParent);
+        CreateItems();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateItems()
     {
-        
+        for (int i = 0; i < _itemSprites.Count; i++)
+        {
+            var singleItemInfo = Instantiate(_singleItemInfoPf, transform);
+            singleItemInfo.Initiliaze(_itemTexts[i],_itemSprites[i]);
+        }
     }
 }
